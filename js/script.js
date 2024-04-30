847a8a4c752171f720f33a25e279e3daf8156b04
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-04-30 18:08:30 +0000

    save

--- js/script.js
+++ js/script.js
@@ -7,50 +7,11 @@
 "use strict";
 
 function myButtonClicked() {
-  const TAX_RATE = 0.18;
 
-  // input
-  const pizzaSize = document.getElementById("pizza-size").value;
-  const numberToppings = document.getElementById("number-toppings").value;
-
-  // process
-  let totalCost;
-
-  if (pizzaSize == "extra large") {
-    totalCost = 10.00;
-
-    if (numberToppings == "1") {
-      totalCost += 1.00;
-    } else if (numberToppings == "2") {
-      totalCost += 1.75;
-    } else if (numberToppings == "3") {
-      totalCost += 2.50;
-    } else if (numberToppings == "4") {
-      totalCost += 3.35;
-    }
-  } else if (pizzaSize == "large") {
-    totalCost = 6.00;
-
-    if (numberToppings == "1") {
-      totalCost += 1.00;
-    } else if (numberToppings == "2") {
-      totalCost += 1.75;
-    } else if (numberToppings == "3") {
-      totalCost += 2.50;
-    } else if (numberToppings == "4") {
-      totalCost += 3.35;
-    }
-  }
-
-  const noTaxCost = (totalCost * (1.00 - TAX_RATE)).toFixed(2);
-  const governmentTax = (totalCost * TAX_RATE).toFixed(2);
-  const totalAmount = (parseFloat(governmentTax) + parseFloat(noTaxCost)).toFixed(2);
+  //input
+  const day = document.getElementById("final-answer").innerHTML
 
   // Output
-  document.getElementById("cost-no-tax").innerHTML =
+  document.getElementById("final-answer").innerHTML =
     "Your cost with no tax is: CAD$" + noTaxCost;
-  document.getElementById("government-tax").innerHTML =
-    "The government tax on your purchase is: CAD$" + governmentTax;
-  document.getElementById("total-amount").innerHTML =
-    "The total amount on your purchase is: CAD$" + totalAmount;
 }
