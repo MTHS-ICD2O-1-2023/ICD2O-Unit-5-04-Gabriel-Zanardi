1e340e5107eeee4c6c87420ad7a0918235163f56
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-05-18 00:25:38 +0000

    fixed js code

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-"use strict";
+"use strict"
 
 function myButtonClicked() {
 
   //input
   const dayOfWeek = document.getElementById("day-of-week").innerHTML
-  const personAge = document.getElementById("person-age").innerHTML
+  const personAge = parseInt(document.getElementById("person-age").value)
   //process
-  if ((dayOfWeek == "Tuesday" || dayOfWeek == "Thursday") || personAge >= 12 && personAge <= 21) {
+  if ((dayOfWeek == "Tuesday" || dayOfWeek == "Thursday") || (personAge >= 12 && personAge <= 21)) {
     // Output
     document.getElementById("final-answer").innerHTML =
       "You have a discount at your admission"
